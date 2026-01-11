e38294b148857884bf68bde64e0fa142d896cec8
corcionenatalie <corcionenatalie@gmail.com>
2026-01-11 18:38:14 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PulsedBeautyApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PulsedBeautyApp />
+  </React.StrictMode>
+);
